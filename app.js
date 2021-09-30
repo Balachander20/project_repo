9d07f5bda574c3fee333fbc29d185fa28b4bb1aa
balachander <balachander7315@gmail.com>
2021-09-30 19:13:39 +0530

    function types

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+// function keyword
+console.log(add(10,20))
 
+function add(a=0,b=0){
+  var sum=a+b
+  return sum
+}
+
+// function expression
+var add=function(a=0,b=0){
+  var sum=a+b
+  return sum
+}
+console.log(add(10,20))
+
+// arrow function
+var add= (a=0,b=0)=>{
+  var sum=a+b
+  return sum
+}
+console.log(add(10,20))
