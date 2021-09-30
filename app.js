910debdf300a46c4a1e5cf1f42f5b23b86db9a39
balachander <balachander7315@gmail.com>
2021-09-30 19:13:15 +0530

    function types

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-var list =document.querySelector('ul')
-arr.push['hgj']
 
-var arr=["balu","abhi"]
-
-for (var i=0;i<arr.length;i++)
-{
-
- var element = document.createElement('li')
- var textnode = document.createTextNode(arr[i])
- element.appendChild(textnode)
-list.appendChild(element)
-}
