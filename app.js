ba70d10b7348c9917bacf5daa332f78b675e0a0c
balachander <balachander7315@gmail.com>
2021-10-05 19:20:53 +0530

    promise topic

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-console.log("one");
-console.log("one");
-console.log("one");
-setTimeout(function timeout(){
-    console.log("timer1");
-},2000)
-setTimeout(function timeout(){
-    console.log("timer2");
-},1000)
-console.log("one");
-console.log("one");
-console.log("one");
-console.log("one");
-console.log("one");
-console.log("one");
-console.log("one");
-console.log("one");
+
+let value=new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    resolve('hello')
+  },3000)
+
+  
+})
+console.log(value)
+setTimeout(()=>{
+  console.log(value)
+},5000)
+
+
