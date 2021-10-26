55b5f262e9afb5a1859b8c8e31597c771942b00b
balachander <balachander7315@gmail.com>
2021-10-26 19:32:39 +0530

    fsmodule

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const path =require("path")
+// readfile
+const fs =require("fs")
+fs.readFile("text.txt","hex",(error,data)=>{
+  if(error)console.log(error)
+  else
+  console.log(data.toString())
 
-console.log(path.resolve("index.js"))
-console.log(path.dirname("./vscode/index.js"))
-console.log(__dirname)
+})
+// fsreadfile
+const g =require("fs")
+
+console.log(g.readFileSync("text.txt","utf-8"))
+console.log("baluuaakj")
+
+
