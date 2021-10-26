f8a5d18bce495024f5a3d449706531c5e69da504
balachander <balachander7315@gmail.com>
2021-10-26 08:09:47 +0530

    pthmodule

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const b=document.querySelector("button")
-const i=document.querySelector("input")
-const d=document.querySelector("div")
-const a=[1,2,3,4,5]
-localStorage.setItem("val",JSON.stringify(a))
-console.log(JSON.parse(localStorage.getItem("val")))
+const path =require("path")
+
+console.log(path.resolve("index.js"))
+console.log(path.dirname("./vscode/index.js"))
+console.log(__dirname)
