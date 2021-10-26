eef103199dd25c8daca3e320fd62eeddf11ac663
balachander <balachander7315@gmail.com>
2021-10-26 07:16:45 +0530

    localstorage

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-
-const arr=[24,2763,27,652,26]
-const  val = JSON.stringify(arr)
-console.log(arr)
-console.log(val)
-
+const b=document.querySelector("button")
+const i=document.querySelector("input")
+const d=document.querySelector("div")
+const a=[1,2,3,4,5]
+localStorage.setItem("val",JSON.stringify(a))
+console.log(JSON.parse(localStorage.getItem("val")))
